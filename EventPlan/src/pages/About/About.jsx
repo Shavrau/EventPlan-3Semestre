040f663b3a3f955c63112316e258c5e57ba3d18f
pedrobiondi23 <pedro.carvalho32@fatec.sp.gov.br>
2024-06-14 22:34:45 -0300

    AtualizaçãoSobre

--- EventPlan/src/pages/About/About.jsx
+++ EventPlan/src/pages/About/About.jsx
@@ -4,6 +4,9 @@ import rhian from "../../assets/rhian.jpeg"
 import Cleston from "../../assets/Cleston.jpeg"
 import Felipe from "../../assets/Felipe.jpeg"
 import Pedro from "../../assets/Pedro.jpeg"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NavLink } from 'react-router-dom';
+
 
 const About = () => {
   return (
@@ -18,20 +21,26 @@ const About = () => {
               Somos estudantes da Fatec Matão, desenvolvendo o trabalho interdisciplinar do curso de DSM. Temos a intensão de aumentar nossos conhecimentos com este trabalho, visando evoluir e aprender mais sobre a área.
               Nossa plataforma tem em vista ser intuitiva com acesso seguro nas transações efetuadas dentro do site, facilitar a venda de ingressos para os eventos em nossa região, além de ser compatível com a maioria dos dispositivos.
             </p>
+            </div>
           </div>
-          <div className={styles.col}>
+          <div className={styles.Nome}>
             <img src={rhian} alt="foto do membro do grupo Rhian" className={styles["foto-sobre"]} />
             <div className={styles.Nome}>Rhian Mendes Souza</div>
             <img src={Cleston} alt="foto do membro do grupo Cleston" className={styles["foto-sobre"]} />
             <div className={styles.Nome}>Cleston Kenji Maruyama Tonooka</div>
-          </div>
-          <div className={styles.col}>
+          <div className={styles.Nome}>
             <img src={Felipe} alt="foto do membro do grupo Felipe" className={styles["foto-sobre"]} />
             <div className={styles.Nome}>Felipe Salgaço Moretti</div>
             <img src={Pedro} alt="foto do membro do grupo Pedro" className={styles["foto-sobre"]} />
             <div className={styles.Nome}>Pedro Henrique Biondi de Carvalho</div>
           </div>  
         </div>
+        <NavLink to="../Home">
+          <div className={styles.content6}>
+          <FontAwesomeIcon icon="fa-solid fa-right-to-bracket" className={styles.iconevoltar} />
+          VOLTAR
+          </div>
+          </NavLink>
       </main>
     </>
   );
